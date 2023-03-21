c4e3b1710002db4e461e5430d7ff67034a585eb3
Joao Vitor <joaovitorparussoloalbuquerque@gmail.com>
2023-03-21 17:55:01 -0300

    first commit

---
+++ app/src/mocks/categories.ts
@@ -0,0 +1,22 @@
+export const categories = [
+  {
+    _id: '6415bbc86e7feb0968e8a055',
+    name: 'Pizza',
+    icon: '🍕',
+  },
+  {
+    _id: '6415bc2c6e7feb0968e8a05a',
+    name: 'Bebidas',
+    icon: '🍻',
+  },
+  {
+    _id: '6415bc0a6e7feb0968e8a058',
+    name: 'Burgers',
+    icon: '🍔',
+  },
+  {
+    _id: '6415bc616e7feb0968e8a05c',
+    name: 'Promoções',
+    icon: '🏷',
+  },
+];
---
+++ app/src/mocks/products.ts
@@ -0,0 +1,47 @@
+export const products = [
+  {
+    _id: '6415bf3f6e7feb0968e8a071',
+    name: 'Pizza quatro queijos',
+    description: 'Deliciosa pizza quatro queijos com borda simples',
+    imagePath: '1679146566805-quatro-queijos.png',
+    price: 40,
+    ingredients: [
+      {
+        name: 'Mussarela',
+        icon: '🧀',
+        _id: '6372e040f52e37ef85fe2c5f'
+      },
+      {
+        name: 'Parmesão',
+        icon: '🧀',
+        _id: '6372e040f52e37ef85fe2c60'
+      },
+      {
+        name: 'Gouda',
+        icon: '🧀',
+        _id: '6372e040f52e37ef85fe2c61'
+      },
+      {
+        name: 'Brie',
+        icon: '🧀',
+        _id: '6372e040f52e37ef85fe2c62'
+      }
+    ],
+  },
+  {
+    _id: '6415bf616e7feb0968e8a07b',
+    name: 'Coca cola',
+    description: 'Coca cola lata',
+    imagePath: '1679146626460-coca-cola.png',
+    price: 7,
+    ingredients: [],
+  },
+  {
+		_id: '64166a8c991c5435b33265f4',
+		name: 'Cerveja',
+		description: 'Cerveja 1 Litro',
+		imagePath: '1679190667949-cerveja.png',
+		price: 8,
+		ingredients: [],
+	}
+]
